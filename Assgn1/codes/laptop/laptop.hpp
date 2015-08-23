32877b4bbe51650d7dd17ab54943f51eefbfb720
Pradyot Prakash <pp2105@gmail.com>
2015-08-23 14:53:07 +0530

    Everything except modelling done

--- Assgn1/codes/laptop/laptop.hpp
+++ Assgn1/codes/laptop/laptop.hpp
@@ -20,6 +20,8 @@ GLfloat xpos=0.0,ypos=0.0,zpos=0.0;
 GLfloat xrot=0.0,yrot=0.0,zrot=0.0;
 // File read write parameters
 GLboolean file_write = 0, file_load = 0, centroid_translate = 0;
+// modelling/inspection control
+GLboolean modelling_enabled = 0, inspection_enabled = 1;
 
 //Running variable to toggle culling on/off
 bool enable_culling=true;
